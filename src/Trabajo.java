a0c41bebe48921057c4244a0061db7c07cf9eebd
Xisco <xcambil@gmail.com>
2016-03-11 00:49:23 +0100

    nuevas funciones añadidas

--- src/Trabajo.java
+++ src/Trabajo.java
@@ -15,60 +15,108 @@ public class Trabajo {
                 "#     #\n" +
                 "#######";
         Bender b = new Bender(mapa);
+           System.out.print(b);
 
 
     }
 
 }
-    class Bender {
-        private char[][] tablero;
-        private int AlturaMapa;
-        private int AnchoMapa;
 
-        public Bender(String mapa) {
-            AlturaMapa = NumeroLineas(mapa);
-            AnchoMapa = ContarCaracter(mapa);
-            tablero = CrearMapa(mapa, AlturaMapa, AnchoMapa);
-        }
-
-        private int NumeroLineas(String mapa){
-            int contador = 0;
-            for (int i = 0; i < mapa.length(); i++) {
-                if (mapa.charAt(i) == '\n') {
-                    contador++;
-
-                }
-            }
-            contador++;
-            return contador;
-        }
-
-        private int ContarCaracter(String mapa){
-            int i = 0;
-            int caracter = 0;
-            while(mapa.charAt(i) != '\n'){
-
-                caracter++;
-                i++;
-            }
-            return caracter;
-        }
-
-        private char[][] CrearMapa(String mapa , int altura,int anchura){
-            tablero = new char[altura][anchura];
-            int p = 0;
-            for (int i = 0; i < altura ; i++) {
-                for (int j = 0; j < anchura; j++) {
-                    tablero[i][j] = mapa.charAt(p);
-
-                }
+class Bender {
+    private char[][] tablero;
+    private final int alturaMapa;
+    private final int anchoMapa;
+    private final String mapa;
 
 
-            }
+    public Bender(String mapa) {
+        this.mapa = mapa;
+        alturaMapa = CalcularAltoMapa();
+        anchoMapa = CalcularAnchoMapa();
+        tablero = CrearMapa();
 
-return null;
-        }
 
     }
 
+    private int CalcularAltoMapa() {
+        int contador = 0;
+        for (int i = 0; i < mapa.length(); i++) {
+            if (mapa.charAt(i) == '\n') {
+                contador++;
 
+            }
+        }
+        contador++;
+        return contador;
+    }
+
+    private int CalcularAnchoMapa() {
+        int i = 0;
+        int caracter = 0;
+        while (mapa.charAt(i) != '\n') {
+
+            caracter++;
+            i++;
+        }
+        return caracter;
+    }
+
+    private char[][] CrearMapa() {
+        tablero = new char[alturaMapa][anchoMapa];
+        int linea = 0;
+        for (int i = 0; i < alturaMapa; i++) {
+            for (int j = 0, c = 0; j < anchoMapa; j++, c++) {
+                tablero[i][j] = mapa.charAt(c + linea);
+            }
+            linea += anchoMapa + 1;
+        }
+        return tablero;
+    }
+
+    public String run() {
+        boolean ganar = false;
+        while (!ganar) {
+
+
+
+        }
+
+        return "";
+    }
+
+
+    private int[] PosicionX() {
+        int[] cordenadas = new int[2];
+        salir:
+        for (int i = 0; i < alturaMapa; i++) {
+            for (int j = 0; j < anchoMapa; j++) {
+                if (tablero[i][j] != 'X') {
+                    continue;
+                } else {
+                    cordenadas[0] = i;
+                    cordenadas[1] = j;
+                    break salir;
+                }
+            }
+
+        }
+        return cordenadas;
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        String resultado = "";
+        for (int i = 0; i < alturaMapa; i++) {
+            for (int j = 0; j < anchoMapa; j++) {
+                resultado += tablero[i][j];
+            }
+            resultado += '\n';
+        }
+        return resultado;
+    }
+
+
+}
